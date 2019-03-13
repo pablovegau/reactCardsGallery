62a249ff4030f480efd80a0fcec754183b29bf2d
Pablo Vega Uceta <pablo.vega@scmspain.com>
2019-03-13 20:19:30 +0100

    Add React and Babel and its configurations

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
 const loadPresets = require('./build-utils/loadPresets');
 
 const pathsToClean = { template: './dist' };
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: './src/index.html',
+  filename: './index.html',
+});
 
 module.exports = ({ mode, presets } = { mode: 'production', presets: [] }) =>
   webpackMerge(
@@ -24,12 +29,19 @@ module.exports = ({ mode, presets } = { mode: 'production', presets: [] }) =>
               },
             ],
           },
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+            },
+          },
         ],
       },
       output: {
         filename: 'bundle.js',
       },
-      plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin(), new CleanWebpackPlugin(pathsToClean)],
+      plugins: [htmlPlugin, new webpack.ProgressPlugin(), new CleanWebpackPlugin(pathsToClean)],
     },
     modeConfig(mode),
     loadPresets({ mode, presets }),
