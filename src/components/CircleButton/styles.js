6b36a46f30a9e37ca07cc50a7c0c68161d55923f
Pablo Vega Uceta <pablo.vega@scmspain.com>
2019-03-14 22:50:18 +0100

    Circle button related styles with variables

--- src/components/CircleButton/styles.js
+++ src/components/CircleButton/styles.js
@@ -2,6 +2,12 @@ import styled from 'styled-components';
 
 import { appColors, appGrayColors } from '../../theme/colors';
 
+const circleButton_borderRadius = '50%';
+const circleButton_height = '4rem';
+const circleButton_width = circleButton_height;
+const circleButton_colorTransitionTime = '0.6s';
+const icon_height = '2rem';
+
 const getComputedStyles = ({ secondary }) => `
   background-color: ${secondary ? appGrayColors.black40 : appColors.primary};
 `;
@@ -11,10 +17,10 @@ export const CircleButtonWrapper = styled.button`
   align-items: center;
   justify-content: center;
   border: none;
-  border-radius: 50%;
-  height: 4rem;
-  width: 4rem;
-  transition: background-color 0.6s;
+  border-radius: ${circleButton_borderRadius};
+  height: ${circleButton_height};
+  width: ${circleButton_width};
+  transition: background-color ${circleButton_colorTransitionTime};
   ${() => getComputedStyles}
 
   &:hover {
@@ -23,7 +29,7 @@ export const CircleButtonWrapper = styled.button`
 
   svg {
     display: block;
-    height: 2rem;
+    height: ${icon_height};
   }
 
   path {
--- src/containers/ReactCardsGallery/styles.js
+++ src/containers/ReactCardsGallery/styles.js
@@ -2,15 +2,20 @@ import styled from 'styled-components';
 
 import { appGrayColors } from '../../theme/colors';
 
+const reactCardsGallery_width = '100vw';
+const reactCardsGallery_height = '100vh';
+const button_bottom = '2rem';
+const button_right = button_bottom;
+
 export const Wrapper = styled.div`
   position: relative;
-  width: 100vw;
-  height: 100vh;
+  width: ${reactCardsGallery_width};
+  height: ${reactCardsGallery_height};
   background-color: ${appGrayColors.black10};
 
   .circleButton {
     position: absolute;
-    bottom: 2rem;
-    right: 2rem;
+    bottom: ${button_bottom};
+    right: ${button_right};
   }
 `;
