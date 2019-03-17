af8bddc183ec427e4ce99710b6e9e5261b5dc092
Pablo Vega Uceta <pablo.vega@scmspain.com>
2019-03-17 09:57:26 +0100

    Modify components Button received props

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -4,12 +4,13 @@ import PropTypes from 'prop-types';
 import { Button } from './styles';
 
 const button = props => {
-  const { label } = props;
-  return <Button>{label}</Button>;
+  const { label, position } = props;
+  return <Button position={position}>{label}</Button>;
 };
 
 button.propTypes = {
   label: PropTypes.string.isRequired,
+  position: PropTypes.string,
 };
 
 export default button;
--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -2,6 +2,24 @@ import styled from 'styled-components';
 
 import { appColors, appPadding, appFontSizes, appGrayColors } from '../../theme';
 
+const getPositionParameters = ({ position }) => {
+  switch (position) {
+    case 'center':
+      return `
+        margin-left: auto;
+        margin-right: auto;
+      `;
+    case 'right':
+      return `
+        margin-left: auto;
+        margin-right: 0;
+      `;
+
+    default:
+      return null;
+  }
+};
+
 const button_horizontalpPadding = appPadding.m;
 const button_verticalPadding = appPadding.s;
 const button_fontSize = appFontSizes.xl;
@@ -9,10 +27,12 @@ const button_fontColor = appGrayColors.white;
 const button_borderRadius = '.3rem';
 
 export const Button = styled.button`
+  display: block;
   padding: ${button_verticalPadding} ${button_horizontalpPadding};
   border-radius: ${button_borderRadius};
   background-color: ${appColors.primary};
   color: ${button_fontColor};
   font-size: ${button_fontSize};
   text-transform: uppercase;
+  ${props => getPositionParameters({ position: props.position })}
 `;
