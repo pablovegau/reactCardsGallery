b5545b1cd7d8124e386d776fe2674ff707d7360d
Pablo Vega Uceta <pablo.vega@scmspain.com>
2019-03-17 11:50:56 +0100

    Create modal component

--- src/containers/ReactCardsGallery/index.js
+++ src/containers/ReactCardsGallery/index.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import CircleButton from '../../components/CircleButton';
+import Modal from '../Modal';
 import plusIcon from '../../assets/svg/paths/plusIcon';
 
 import { Wrapper, WrapperCircleButton } from './styles';
 
 class ReactCardsGallery extends Component {
+  state = {
+    showModal: false,
+  };
+
+  onShowModal = () => {
+    this.setState({
+      showModal: !this.state.showModal,
+    });
+  };
+
   render() {
     return (
       <Wrapper>
-        <WrapperCircleButton>
+        <WrapperCircleButton onClick={this.onShowModal}>
           <CircleButton path={plusIcon} secondary={false} />
         </WrapperCircleButton>
+        {this.state.showModal ? <Modal showModal={this.state.showModal} clickVeil={this.onShowModal} /> : null}
       </Wrapper>
     );
   }
